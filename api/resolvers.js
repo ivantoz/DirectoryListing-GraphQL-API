0b15f2ff50379cb5a3d4e7beb177fbb5c4bec6b3
ikipyegon <ikipyegon@safaricom.co.ke>
2021-03-24 18:17:30 +0300

    add more attributes to directory and file resolver

--- api/resolvers.js
+++ api/resolvers.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require("path")
 const dree = require('dree');
 
 const options = {
@@ -18,8 +19,27 @@ const options = {
 class TreeNode {
     constructor(path) {
         this.path = path;
+        this.type = ""
+        this.size = ""
+        this.extension = ""
         this.children = [];
     }
+}
+
+const convertBytes = function(bytes) {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
+
+    if (bytes == 0) {
+        return "n/a"
+    }
+
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+
+    if (i == 0) {
+        return bytes + " " + sizes[i]
+    }
+
+    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i]
 }
 
 function buildTree(rootPath) {
@@ -37,6 +57,34 @@ function buildTree(rootPath) {
                 for (let child of children) {
                     const childPath = `${currentNode.path}/${child}`;
                     const childNode = new TreeNode(childPath);
+                    let totalSize = 0
+                    if (fs.statSync(currentNode.path).isDirectory()) {
+                        totalSize += fs.statSync(currentNode.path).size
+                        currentNode.size = convertBytes(totalSize)
+                        currentNode.type = "Directory"
+                        currentNode.extension = "n/a"
+                    }
+
+                    if (fs.statSync(currentNode.path).isFile()) {
+                        currentNode.extension = path.extname(currentNode.path);
+                        currentNode.type = "File"
+                    }
+
+                    let childTotalSize = 0
+
+                    if (fs.statSync(childNode.path).isDirectory()) {
+                        childTotalSize += fs.statSync(childNode.path).size
+                        childNode.size = convertBytes(childTotalSize)
+                        childNode.extension = "n/a"
+                        childNode.type = "Directory"
+                    }
+
+                    if (fs.statSync(childNode.path).isFile()) {
+                        childNode.size = convertBytes(fs.statSync(childNode.path).size)
+                        childNode.extension = path.extname(childNode.path);
+                        childNode.type = "File"
+                    }
+
                     currentNode.children.push(childNode);
 
                     if (fs.statSync(childNode.path).isDirectory()) {
@@ -57,6 +105,9 @@ const resolvers = {
         directoryListing: (root, {rootPath}) => {
            //return buildTree(rootPath)
             return dree.scan(rootPath, options);
+        },
+        listing: (root, {rootPath}) => {
+            return buildTree(rootPath)
         }
     }
 }
