292bcf13fa1d4131729471e65cb798d1b07850a8
ikipyegon <ikipyegon@safaricom.co.ke>
2021-03-24 19:51:02 +0300

    add eslint configs for linting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    parser: "babel-eslint",
+    rules: {
+        "graphql/template-strings": ['error', {
+            env: 'apollo',
+        }]
+    },
+    "env": {
+        "es6": true,
+        "node": true,
+    },
+    plugins: [
+        'graphql',
+        // '@babel'
+    ]
+}
